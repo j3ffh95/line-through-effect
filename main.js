838824e89f0559649c3f635347b227ece994c51e
j3ffh95 <j3ffh95@gmail.com>
2020-08-08 14:14:28 -0400

    append new element to body

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const text = document.querySelector("#text");
 
 const textArray = text.innerText.split("");
+
+const newEl = document.createElement("h1");
+newEl.innerHTML = `
+  ${textArray
+    .map((letter) => `<span style="${randomVisibility()}">${letter}</span>`)
+    .join("")}
+`;
+
+newEl.classList.add("overlay");
+
+document.body.appendChild(newEl);
